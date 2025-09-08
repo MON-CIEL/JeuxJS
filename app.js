c30bdf482e94a27882a69cada52e37df60c04945
Marcelo Monteiro <mamonteirogarcia@gmail.com>
2025-09-08 16:20:00 +0200

    Commit #2 "Création du serveur Web en Node.js"

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 'use strict';
 
 console.log('TP CIEL');
+
+/*  *********************** Serveur Web ***************************   */
+// 
+var express = require('express');
+
+var exp = express();
+
+var port=80; /* Port du Site*/
+
+exp.use(express.static('D:/monteirom.SNIRW/TP20_Javascript/JeuxJS/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile('D:/monteirom.SNIRW/TP20_Javascript/JeuxJS/www/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+}); 
